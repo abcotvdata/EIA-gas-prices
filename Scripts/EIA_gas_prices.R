f8baaec3f297db2d17355faf01673a373d484718
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2022-05-11 16:30:39 -0700

    Create EIA_gas_prices.R

---
+++ Scripts/EIA_gas_prices.R
@@ -0,0 +1,53 @@
+# use EIA library key
+
+eia_set_key(EIA_key)
+
+# San Francisco
+
+id <- "PET.EMM_EPMR_PTE_Y05SF_DPG.W"
+(d <- eia_series(id, n = 100))
+
+sf_gas <- unnest(d)
+
+sf_gas1 <- sf_gas %>% 
+  select("name", "updated", "value", "date", "year", "month", "week")
+
+write.csv(sf_gas1,"sf_gas.csv", row.names = TRUE)
+
+# Los Angeles
+
+id <- "PET.EMM_EPMR_PTE_Y05LA_DPG.W"
+(d <- eia_series(id, n = 100))
+
+la_gas <- unnest(d)
+
+la_gas1 <- la_gas %>% 
+  select("name", "updated", "value", "date", "year", "month", "week")
+
+write.csv(la_gas1,"la_gas.csv", row.names = TRUE)
+
+# Chicago
+
+id <- "PET.EMM_EPMR_PTE_YORD_DPG.W"
+(d <- eia_series(id, n = 100))
+
+chicago_gas <- unnest(d)
+
+chicago_gas1 <- chicago_gas %>% 
+  select("name", "updated", "value", "date", "year", "month", "week")
+
+write.csv(chicago_gas1,"chicago_gas.csv", row.names = TRUE)
+
+# New York City
+
+id <- "PET.EMM_EPMR_PTE_Y35NY_DPG.W"
+(d <- eia_series(id, n = 100))
+
+nyc_gas <- unnest(d)
+
+nyc_gas1 <- nyc_gas %>% 
+  select("name", "updated", "value", "date", "year", "month", "week")
+
+write.csv(nyc_gas1,"nyc_gas.csv", row.names = TRUE)
+
+# Philadelphia is not listed 
